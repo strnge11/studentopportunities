15440027bcbdf6330e9bec79e02cb97b58ffbfc2
strnge11 <64690906+strnge11@users.noreply.github.com>
2020-05-02 21:58:18 +0800

    Update incon.php

--- Project/incon.php
+++ Project/incon.php
@@ -13,7 +13,7 @@
 	$guardno 	= $_POST['guardno'];
 
 	//Database connection
-	$conn = new mysqli('localhost', 'root', '', 'instudex');
+	$conn = new mysqli('localhost', 'pdcazambdb', 'T2kWPWrwgN', 'pdcazamdb');
 	if($conn->connect_error){
 		die('Connection Failed: ' .$conn->connect_error);
 	}else{
