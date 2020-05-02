bfaca62438556c3e8a270b8b0c2c0cc2497f08f2
strnge11 <64690906+strnge11@users.noreply.github.com>
2020-05-02 22:20:39 +0800

    Add files via upload

---
+++ outcon.php
@@ -0,0 +1,26 @@
+<?php
+	$studname	= $_POST['studname'];
+	$studno 	= $_POST['studno'];
+	$cellnum 	= $_POST['cellnum'];
+	$course 	= $_POST['course'];
+	$email 		= $_POST['email'];
+	$term 		= $_POST['term'];
+	$graddate 	= $_POST['graddate'];
+	$univ 		= $_POST['univ'];
+	$dest 		= $_POST['dest'];
+	$units 		= $_POST['units'];
+	$accom 		= $_POST['accom'];
+
+	//Database connection
+	$conn = new mysqli('localhost', 'pdcazamdb', 'T2kWPWrwgN', 'instudex');
+	if($conn->connect_error){
+		die('Connection Failed: ' .$conn->connect_error);
+	}else{
+		$stmt = $conn->prepare("INSERT into outstud (studname, studno, cellnum, course, email, term, graddate, univ, dest, units, accom) VALUES(?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)");
+		$stmt->bind_param("siissssssis", $studname, $studno, $cellnum, $course, $email, $term, $graddate, $univ, $dest, $units, $accom);
+		$stmt->execute();
+		echo"Registration successfull.";
+		$stmt->close();
+		$conn->close();	
+	}
+?>
---
+++ scholarcon.php
@@ -0,0 +1,34 @@
+<?php
+	$scho			= $_POST['scho'];
+	$surname 		= $_POST['surname'];
+	$firstname 		= $_POST['firstname'];
+	$midname 		= $_POST['midname'];
+	$studno 		= $_POST['studno'];
+	$program 		= $_POST['program'];
+	$existing 		= $_POST['existing'];
+	$remunits 		= $_POST['remunits'];
+	$remterms 		= $_POST['remterms'];
+	$dateofbirth 	= $_POST['dateofbirth'];
+	$age 			= $_POST['age'];
+	$placeofbirth 	= $_POST['placeofbirth'];
+	$email 			= $_POST['email'];
+	$citizenship 	= $_POST['citizenship'];
+	$gender 		= $_POST['gender'];
+	$civilstat 		= $_POST['civilstat'];
+	$religion 		= $_POST['religion']; 
+	$address 		= $_POST['address']; 
+	$contact 		= $_POST['contact']; 
+
+	//Database connection
+	$conn = new mysqli('localhost', 'pdcazamdb', 'T2kWPWrwgN', 'instudex');
+	if($conn->connect_error){
+		die('Connection Failed: ' .$conn->connect_error);
+	}else{
+		$stmt = $conn->prepare("INSERT into scholar (scho, surname, firstname, midname, studno, program, existing, remunits, remterms, dateofbirth, age, placeofbirth, email, citizenship, gender, civilstat, religion, address, contact) VALUES(?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)");
+		$stmt->bind_param("ssssissiisisssssssi", $scho, $surname, $firstname, $midname, $studno, $program, $existing, $remunits, $remterms, $dateofbirth, $age, $placeofbirth, $email, $citizenship, $gender, $civilstat, $religion, $address, $contact);
+		$stmt->execute();
+		echo"Registration successfull.";
+		$stmt->close();
+		$conn->close();	
+	}
+?>
